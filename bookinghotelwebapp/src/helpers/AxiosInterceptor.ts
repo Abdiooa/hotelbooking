663f3444a0f4a100dd0aa278b6723bf86c30a5b1
Abdiooa <abdiooa45@gmail.com>
2024-05-27 19:50:34 +0300

    added some changes

--- bookinghotelwebapp/src/helpers/AxiosInterceptor.ts
+++ bookinghotelwebapp/src/helpers/AxiosInterceptor.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8070";
+const BASE_URL = "http://hotelbookingbackendapp:8070";
 const axiosHttp = axios.create({
-    baseURL: `${BASE_URL}`,
+  baseURL: `${BASE_URL}`,
 });
 
 axiosHttp.interceptors.request.use(
-    (request) => request,
-    (error) => Promise.reject(error)
+  (request) => request,
+  (error) => Promise.reject(error)
 );
-  
+
 axiosHttp.interceptors.response.use(
-    (response) => response,
-    (error) => Promise.reject(error)
+  (response) => response,
+  (error) => Promise.reject(error)
 );
-  
+
 export default axiosHttp;
